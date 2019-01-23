d0581c7d107d0816072364ec485e971fb2b364d7
illutek <stefanvandenborne@gmail.com>
2019-01-23 22:46:39 +0100

    grid__wrapper to grid__row

--- js/menu.js
+++ js/menu.js
@@ -9,7 +9,7 @@
   const openBtn = document.querySelector('.hamburger__button');
   const closeBtn = document.querySelector('.close__button');
   const menuOverlay = document.querySelector('.menu-overlay');
-  const navTopHidden = document.querySelector('.navigation');
+  const navTopHidden = document.querySelector('.nav--top');
 
   // console.log(closeBtn);
 
@@ -19,12 +19,12 @@
 
   function closeMenu() {
     menuOverlay.classList.remove('menu-overlay--visible');
-    navTopHidden.classList.remove('navigation--hidden');
+    navTopHidden.classList.remove('nav--hidden');
   }
 
   function openMenu() {
     menuOverlay.classList.toggle('menu-overlay--visible');
-    navTopHidden.classList.add('navigation--hidden');
+    navTopHidden.classList.add('nav--hidden');
   }
 
   openBtn.addEventListener('click', openMenu);
